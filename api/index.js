6322714bd075635fdecaa59c8e352594d90355d3
Zentaiva <flundi.floundy90@gmail.com>
2026-06-12 13:38:54 +0500

    debug: show error in response

--- api/index.js
+++ api/index.js
@@ -4,8 +4,11 @@ module.exports = async (req, res) => {
   try {
     await handle(req, res);
   } catch (err) {
-    console.error(err);
+    console.error('=== CRASH ===');
+    console.error(err.name, err.message);
+    console.error(err.stack);
     res.statusCode = 500;
-    res.end('Internal Server Error');
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`Error: ${err.name}: ${err.message}\n\n${err.stack}`);
   }
 };
